68033458945e380d491a9a1cb79c55fa2ebef22f
Christopher Falkner <50120458+falkn111@users.noreply.github.com>
2020-11-26 16:23:01 -0500

    Update and rename CSVReader to CSVReader.java

--- CSVReader.java
+++ CSVReader.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+import java.io.File;  
+import java.io.FileNotFoundException;     
+
 class CSVReader extends FileReader
 {
+    //Calls FileReaders Constructor
     public CSVReader()
     {
         super();
+    }
+
+    //Calls FileReaders Overloaded Constructor
+    public CSVReader(String FileName)
+    {
+        super(FileName);
     }
 
     //Displays contents of a given file
@@ -18,6 +29,8 @@ class CSVReader extends FileReader
             while (Reader.hasNextLine()) 
             {
                 String line = Reader.nextLine();    //Reads line in file
+                line = line.replace(","," | ");
+                line = line.replace("\"","");
                 FileContent.append(line + "\n");    //Stores multiple lines
             }
             Reader.close();
